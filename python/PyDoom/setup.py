c9e957a108aaa95575bcceaacbbb4304fed5e54f
Mathieu-Robitaille <mathieujrobitaille@gmail.com>
2019-11-20 10:56:26 -0500

    Created doc string

--- python/PyDoom/setup.py
+++ python/PyDoom/setup.py
@@ -1,5 +1,11 @@
+"""
+The entry point for this project.
+Enter this directory and run
+"pip install -r requirements.txt"
+"python ./setup.py"
+"""
+
 import PyDoom.main
 
 if __name__ == "__main__":
     PyDoom.main.main()
-
