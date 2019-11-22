631f393d6e2175794d5128bfc8554e63f6093c34
Mathieu-Robitaille <mathieujrobitaille@gmail.com>
2019-11-22 09:20:24 -0500

    rebuilding DoomCAD, it hurts to read

--- python/PyDoom/DoomCAD/doomcad.py
+++ python/PyDoom/DoomCAD/doomcad.py
@@ -6,14 +6,19 @@ import pygame as pg
 DoomCAD is 100% still in the "This is a rough prototype phase" so messy code is to be expected as
 it is very possible I abandon this in favor of a 3D level editor. Especially if I migrate to pandas3D
 for rendering in the future
+
+As of Nov 22 2019, This project's code has grown to be very gross. Please do not look at my shame.  
 """
 
 CLOSE = 4
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+# Random keys, these are probably really bad picks, but i want to see the code actually work
+# IF it works as i want, i can easily change these to a sensible set later
 PAN_KEY = pg.K_LCTRL
 RECTANGLE_KEY = pg.K_LSHIFT
 CIRCLE_KEY = pg.K_c
+SPLINE_KEY = pg.K_x
 
 
 class DoomCAD:
@@ -27,7 +32,7 @@ class DoomCAD:
         self.offset = (-SCREEN_WIDTH / 2, -SCREEN_HEIGHT / 2)
         self.start_offset = (0, 0)
         # Line, Rectangle, Circle, Splines
-        self.shapes = [False, False, False, False]
+        self.shapes = [False for i in range(4)]
         self.mouse_pos = (0, 0)
         self.lines = []
         self.squares = []
@@ -35,26 +40,11 @@ class DoomCAD:
         self.pan = False
 
     def draw(self):
-        if self.shapes[1]:
-            pg.draw.circle(self.surface, pg.Color("White"), pg.mouse.get_pos(), 20)
+        # Display  the center of the screen so we know where we are when panning
         pg.draw.circle(self.surface, pg.Color("white"), world_to_screen((0, 0), self.offset), 10)
-        for line in self.lines:
-            start = world_to_screen(line[0], self.offset)
-            end = world_to_screen(line[1], self.offset)
-            pg.draw.aaline(self.surface, pg.Color("White"), start, end)
-            if dist(start, pg.mouse.get_pos()) <= CLOSE:
-                pg.draw.circle(self.surface, pg.Color("Red"), start, 3)
-            if dist(end, pg.mouse.get_pos()) <= CLOSE:
-                pg.draw.circle(self.surface, pg.Color("Red"), end, 3)
-        for rect in self.squares:
-            x, y = world_to_screen((rect.x, rect.y), self.offset)
-            w = rect.w
-            h = rect.h
-            pg.draw.rect(self.surface, pg.Color("Green"), (x, y, w, h), width=5)
-        for circle in self.circles:
-            x, y = world_to_screen((circle.x, circle.y), self.offset)
-            r = dist((x, y), (circle.w, circle.h))
-            pg.draw.circle(self.surface, pg.Color("Purple"), (x, y), r, width=5)
+        self.draw_lines()
+        self.draw_squares()
+        self.draw_circles()
         if self.end != (-1000000, -1000000):
             end = world_to_screen(self.end, self.offset)
             if self.shapes[0]:
@@ -62,8 +52,33 @@ class DoomCAD:
             elif self.shapes[1]:
                 pos = pg.mouse.get_pos()
                 pg.draw.rect(self.surface, pg.Color("Red"), (end[0], end[1], pos[0] - end[0], pos[1] - end[1]), 5)
-
         pg.display.update()
+
+    def draw_lines(self):
+        for line in self.lines:
+            start = world_to_screen(line[0], self.offset)
+            end = world_to_screen(line[1], self.offset)
+            pg.draw.aaline(self.surface, pg.Color("White"), start, end)
+            self.move_dot(start, end)
+
+    def draw_squares(self):
+        for rect in self.squares:
+            x, y = world_to_screen((rect.x, rect.y), self.offset)
+            w = rect.w
+            h = rect.h
+            pg.draw.rect(self.surface, pg.Color("Green"), (x, y, w, h), width=5)
+
+    def draw_circles(self):
+        for circle in self.circles:
+            x, y = world_to_screen((circle.x, circle.y), self.offset)
+            r = dist((x, y), (circle.w, circle.h))
+            pg.draw.circle(self.surface, pg.Color("Purple"), (x, y), r, width=5)
+
+    def move_dot(self, start, end):
+        if dist(start, pg.mouse.get_pos()) <= CLOSE:
+            pg.draw.circle(self.surface, pg.Color("Red"), start, 3)
+        if dist(end, pg.mouse.get_pos()) <= CLOSE:
+            pg.draw.circle(self.surface, pg.Color("Red"), end, 3)
 
     def near_other(self):
         """
@@ -77,10 +92,24 @@ class DoomCAD:
                 return line, line[0]
         return None
 
+    def toggle_shapes(self):
+        self.shapes = [False for i in range(4)]
+        keys = pg.key.get_pressed()
+        if keys != 0:
+            self.shapes[0] = all(k == 0 for k in keys)
+            # I'm thinking this should provide a priority to rectangle over other shapes
+            self.shapes[1] = bool(keys[RECTANGLE_KEY] and all(s == False for s in self.shapes))
+            self.shapes[2] = bool(keys[CIRCLE_KEY] and all(s == False for s in self.shapes))
+            self.shapes[3] = bool(keys[SPLINE_KEY] and all(s == False for s in self.shapes))
+
     def event(self):
+        """
+        Wow this is bad and needs to get ripped out
+        :return:
+        """
+        keys = pg.key.get_pressed()
         for event in pg.event.get():
             self.pan = False
-            keys = pg.key.get_pressed()
             if event.type == pg.KEYDOWN:
                 self.start_offset = self.mouse_pos
             if keys != 0:
@@ -97,12 +126,12 @@ class DoomCAD:
                             if near[0] in self.lines:
                                 self.lines.remove(near[0])
                     if event.button == 1:
-                        if all(k == 0 for k in keys):
-                            self.shapes[0] = True
-                        elif keys[RECTANGLE_KEY]:
+                        if keys[RECTANGLE_KEY]:
                             self.shapes[1] = True
                         elif keys[CIRCLE_KEY]:
                             self.shapes[2] = True
+                        elif all(k == 0 for k in keys):
+                            self.shapes[0] = True
                         if any(self.shapes):
                             self.end = screen_to_world(pg.mouse.get_pos(), self.offset)
                 if event.type == pg.MOUSEBUTTONUP:
@@ -113,18 +142,15 @@ class DoomCAD:
                             pos = screen_to_world(pg.mouse.get_pos(), self.offset)
                             self.squares.append(pg.Rect(self.end[0], self.end[1],
                                                         pos[0] - self.end[0], pos[1] - self.end[1]))
-                            self.shapes[1] = False
                         elif self.shapes[2]:
                             pos = screen_to_world(pg.mouse.get_pos(), self.offset)
                             self.circles.append(pg.Rect(self.end[0], self.end[1],
                                                         pos[0] - self.end[0], pos[1] - self.end[1]))
-                            self.shapes[2] = False
                         self.end = (-1000000, -1000000)
 
 
 def world_to_screen(world, offset):
     """
-
     :param world: world coords
     :param offset: offset value
     :return:
@@ -161,6 +187,7 @@ def main():
     while True:
         cad.clock.tick(60)
         cad.mouse_pos = pg.mouse.get_pos()
+        cad.toggle_shapes()
         cad.surface.fill(pg.Color("black"))
         cad.event()
         if cad.pan:
